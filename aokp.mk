28eb62be3cd38f3133d8c1ca0a592b70326da30e
GMCadiom <gm.cadiom@gmail.com>
2016-09-27 02:46:08 +0200

    Mediatek : Fix Porting Mistake

--- aokp.mk
+++ aokp.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/aokp/configs/common.mk)
 $(call inherit-product, device/mediatek/mt6582/device_mt6582.mk)
 
 # Locales
-PRODUCT_LOCALES := uk_UA ru_RU en_US en_AU en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ pl_PL ja_JP zh_TW zh_CN zh_HK ko_KR nb_NO es_US da_DK$
+PRODUCT_LOCALES := ar_EG uk_UA ru_RU en_US en_AU en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ pl_PL ja_JP zh_TW zh_CN zh_HK ko_KR nb_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG ca_ES en_GB fi_FI hi_IN hr_HR hu_HU in_ID iw_IL lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS vi_VN tl_PH fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN en_XA ar_XB fr_CA km_KH lo_LA ne_NP si_LK mn_MN hy_AM az_AZ ka_GE my_MM mr_IN ml_IN is_IS mk_MK ky_KG eu_ES gl_ES bn_BD ta_IN kn_IN te_IN uz_UZ ur_PK kk_KZ sq_AL gu_IN pa_IN
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
